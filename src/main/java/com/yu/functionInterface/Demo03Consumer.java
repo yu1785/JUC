e6a874f754af242809f5796aeb52f434406fa3a3
yu1785 <861046209@qq.com>
2020-06-16 19:08:24 +0800

    commit 2020.6.16

---
+++ src/main/java/com/yu/functionInterface/Demo03Consumer.java
@@ -0,0 +1,21 @@
+package com.yu.functionInterface;
+
+import java.util.function.Consumer;
+
+/**
+ * Consumer 消费型接口: 只有输入，没有返回值
+ */
+
+public class Demo03Consumer {
+    public static void main(String[] args) {
+//        Consumer<String> consumer = new Consumer<String>() {
+//            @Override
+//            public void accept(String str) {
+//                System.out.println(str);
+//            }
+//        };
+
+        Consumer<String> consumer = (str)->{ System.out.println(str);};
+        consumer.accept("asa");
+    }
+}
